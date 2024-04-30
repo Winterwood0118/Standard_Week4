5790e122f57e3ed2afa83a0b0f5576a2652e7987
WinterWood0118 <dltjdals125@gmail.com>
2024-04-30 16:52:08 +0900

    unknown_item 추가

--- app/src/main/java/com/example/standard_week4/CardAdapter.kt
+++ app/src/main/java/com/example/standard_week4/CardAdapter.kt
@@ -4,12 +4,79 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.standard_week4.databinding.ItemCardBlueBinding
+import com.example.standard_week4.databinding.ItemCardOrangeBinding
+import com.example.standard_week4.databinding.ItemCardSkyBinding
+import com.example.standard_week4.databinding.UnknownItemBinding
 
 class CardAdapter(private val onClick: (Card) -> Unit) :
-    RecyclerView.Adapter<CardAdapter.CardHolder>() {
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     var cardList = listOf<Card>()
 
-    inner class CardHolder(
+    class UnknownViewHolder(
+        binding: UnknownItemBinding
+    ): RecyclerView.ViewHolder(binding.root){
+        fun bind() = Unit
+    }
+    override fun getItemViewType(position: Int): Int {
+        return cardList[position].mode.viewType
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val multiModeViewType = MultiMode.entries.find { it.viewType == viewType }
+        return when (multiModeViewType) {
+            MultiMode.BLUE -> {
+                val view = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.item_card_blue, parent, false)
+                BlueCardHolder(ItemCardBlueBinding.bind(view), onClick)
+            }
+
+            MultiMode.SKY_BLUE -> {
+                val view = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.item_card_sky, parent, false)
+                SkyBlueCardHolder(ItemCardSkyBinding.bind(view), onClick)
+            }
+
+            MultiMode.ORANGE -> {
+                val view = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.item_card_orange, parent, false)
+                OrangeCardHolder(ItemCardOrangeBinding.bind(view), onClick)
+            }
+
+            else -> {
+                val binding = UnknownItemBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+                UnknownViewHolder(binding)
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return cardList.size
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val currentItem = cardList[position]
+        val multiModeViewType = MultiMode.entries.find { it.viewType == currentItem.mode.viewType }
+        when(multiModeViewType){
+            MultiMode.BLUE -> {
+                (holder as BlueCardHolder).bind(currentItem)
+            }
+            MultiMode.SKY_BLUE -> {
+                (holder as SkyBlueCardHolder).bind(currentItem)
+            }
+            MultiMode.ORANGE -> {
+                (holder as OrangeCardHolder).bind(currentItem)
+            }
+            else -> {
+                (holder as UnknownViewHolder).bind()
+            }
+        }
+    }
+
+    class BlueCardHolder(
         private val binding: ItemCardBlueBinding,
         val onClick: (Card) -> Unit
     ) : RecyclerView.ViewHolder(binding.root) {
@@ -36,39 +103,57 @@ class CardAdapter(private val onClick: (Card) -> Unit) :
 
     }
 
-    override fun getItemViewType(position: Int): Int {
-        return cardList[position].mode.viewType
-    }
+    class SkyBlueCardHolder(
+        private val binding: ItemCardSkyBinding,
+        val onClick: (Card) -> Unit
+    ) : RecyclerView.ViewHolder(binding.root) {
+        private var currentCard: Card? = null
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardAdapter.CardHolder {
-        return when (viewType) {
-            MultiMode.BLUE.viewType -> {
-                val view = LayoutInflater.from(parent.context)
-                    .inflate(R.layout.item_card_blue, parent, false)
-                CardHolder(ItemCardBlueBinding.bind(view), onClick)
+        init {
+            itemView.setOnClickListener {
+                currentCard?.let {
+                    onClick(it)
+                }
             }
-
-            MultiMode.SKY_BLUE.viewType -> {
-                val view = LayoutInflater.from(parent.context)
-                    .inflate(R.layout.item_card_sky, parent, false)
-                CardHolder(ItemCardBlueBinding.bind(view), onClick)
-            }
-
-            MultiMode.ORANGE.viewType -> {
-                val view = LayoutInflater.from(parent.context)
-                    .inflate(R.layout.item_card_orange, parent, false)
-                CardHolder(ItemCardBlueBinding.bind(view), onClick)
-            }
-
-            else -> throw IllegalArgumentException("Unknown View Type")
         }
+
+        fun bind(card: Card) {
+            with(binding) {
+                currentCard = card
+                nameTextView.text = card.owner
+                numberTextView.text = card.cardNumber
+                dateTextView.text = card.cardDate
+                val value = "$" + decimalFormat.format(card.cardValue)
+                valueTextView.text = value
+            }
+        }
+
     }
 
-    override fun getItemCount(): Int {
-        return cardList.size
-    }
+    class OrangeCardHolder(
+        private val binding: ItemCardOrangeBinding,
+        val onClick: (Card) -> Unit
+    ) : RecyclerView.ViewHolder(binding.root) {
+        private var currentCard: Card? = null
 
-    override fun onBindViewHolder(holder: CardAdapter.CardHolder, position: Int) {
-        holder.bind(cardList[position])
+        init {
+            itemView.setOnClickListener {
+                currentCard?.let {
+                    onClick(it)
+                }
+            }
+        }
+
+        fun bind(card: Card) {
+            with(binding) {
+                currentCard = card
+                nameTextView.text = card.owner
+                numberTextView.text = card.cardNumber
+                dateTextView.text = card.cardDate
+                val value = "$" + decimalFormat.format(card.cardValue)
+                valueTextView.text = value
+            }
+        }
+
     }
 }
